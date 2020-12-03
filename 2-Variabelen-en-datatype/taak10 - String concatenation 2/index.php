55f760cacaaad4df7b1091e8ce117adcfaad89ed
Kevin Zwolschen <=>
2020-12-03 11:37:39 +0100

    taak 10 String concatenation 2

---
+++ 2-Variabelen-en-datatype/taak10 - String concatenation 2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1 = "<li>item 1</li>";
+$txt2 = "<li>item 2</li>";
+$txt3 = "<li>item 3</li>";
+echo $txt1;
+echo $txt2;
+echo $txt3;
+?>  
+
+</body>
+</html>
