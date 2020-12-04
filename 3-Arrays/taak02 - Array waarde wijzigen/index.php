de2c72b4e61198c4a7f18b91dd2e82cc4999478a
Kevin Zwolschen <=>
2020-12-04 12:15:16 +0100

    taak 02 Array waarde wijzigen

---
+++ 3-Arrays/taak02 - Array waarde wijzigen/index.php
@@ -0,0 +1,11 @@
+<?php
+    $mycatnames =  ["tijger", "mickey", "tijger"];
+    
+    echo "<li>";
+    echo $mycatnames[0];
+    echo "<li>";
+    echo $mycatnames[1];
+    echo "<li>";
+    echo $mycatnames[2];
+    
+?>
