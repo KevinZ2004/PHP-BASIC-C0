0f083f80d85d4c593f60b9bfe12d455b2d5617a5
Kevin Zwolschen <=>
2020-12-04 10:53:58 +0100

    taak 01 Array variabele

---
+++ 3-Arrays/taak01 - Array variabele/index.php
@@ -0,0 +1,11 @@
+<?php
+    $games = ["Spiderman", "Batman", "The Joker"];
+
+    echo "<li>";
+    echo $games[0];
+    echo "<li>";
+    echo $games[1];
+    echo "<li>";
+    echo $games[2];
+    
+?>
