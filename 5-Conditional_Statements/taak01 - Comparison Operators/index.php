1d18daa50f2c726d8a3a95edbfee909fb3942439
Kevin Zwolschen <=>
2020-12-07 12:08:09 +0100

    taak 01 Comparison Operators

---
+++ 5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -0,0 +1,41 @@
+<?php
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 == $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 === $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 != $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 > $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 < $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 <> $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 >= $test2) . "<br>";
+
+$test1 = 12;  
+$test2 = "gewoon tekst";
+
+var_dump($test1 <= $test2) . "<br>";
+?>  
